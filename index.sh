658dce7109ce5f0522297578f0c34131b5275715
Guillaume Gérard <guillaume.gerard88@gmail.com>
2022-09-26 10:25:50 +0200

    chore: detect test dirs automatically

--- index.sh
+++ index.sh
@@ -2,7 +2,7 @@
 
 mkdir -p tmp
 
-DIRS=("03-monolithic")
+DIRS=`find . -maxdepth 1 -type d -regex '\./[0-9][0-9]-.*'`
 FILES=('input.txt' 'pride-and-prejudice.txt' 'test.txt')
 
 for d in ${DIRS[@]} ; do
